1e6935a4ecbf6fd2319a054ec2114ea1464f3d1e
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-20 18:00:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,15 @@ end
 def using_insert(array, element)
   array.insert(4, element)
 end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, element)
+  array.delete(element)
+end
